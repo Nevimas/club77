065aeaed17c507a85257854c03d9391f84d9cca6
Nevimas <71288898+Nevimas@users.noreply.github.com>
2025-03-03 22:57:56 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,3 @@
-function showModal(package) {
-    const modal = document.getElementById('modal');
-    const packageInfo = document.getElementById('package-info');
-    let content;
-
-    if (package === 'VIP') {
-        content = `
-            <li>Exclusive access to the VIP Lounge</li>
-            <li>Priority entrance</li>
-            <li>Premium drink options</li>
-            <li>Private performances</li>
-        `;
-    } else if (package === 'Regular') {
-        content = `
-            <li>General access to the club</li>
-            <li>Variety of drinks</li>
-            <li>Live entertainment</li>
-            <li>Access to all club areas</li>
-        `;
-    }
-
-    packageInfo.innerHTML = content;
-    modal.style.display = "block";
-}
-
-function closeModal() {
-    const modal = document.getElementById('modal');
-    modal.style.display = "none";
-}
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('Club 77 Website Loaded');
+});
