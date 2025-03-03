34edae9f77cf6f22847c80cc80f5d953c2401f89
Nevimas <71288898+Nevimas@users.noreply.github.com>
2025-03-03 22:48:43 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,37 +1,29 @@
-document.addEventListener('DOMContentLoaded', function() {
-    console.log('Club 77 Website Loaded');
-});
+function showModal(package) {
+    const modal = document.getElementById('modal');
+    const packageInfo = document.getElementById('package-info');
+    let content;
 
-function showModal(type) {
-    document.getElementById("myModal").style.display = "block";
-    let title = document.getElementById("modalTitle");
-    let content = document.getElementById("modalContent");
-
-    if (type === 'VIP') {
-        title.innerHTML = "VIP";
-        content.innerHTML = `
-            <li>Exclusive VIP access to all areas</li>
-            <li>Free drinks and cocktails</li>
-            <li>Private table service</li>
-            <li>Meet and greet with top DJs</li>
+    if (package === 'VIP') {
+        content = `
+            <li>Exclusive access to the VIP Lounge</li>
+            <li>Priority entrance</li>
+            <li>Premium drink options</li>
+            <li>Private performances</li>
         `;
-    } else if (type === 'Standard') {
-        title.innerHTML = "Standard";
-        content.innerHTML = `
-            <li>Standard entry access</li>
-            <li>Access to main dancefloor</li>
-            <li>Complimentary drink</li>
-        `;
-    } else if (type === 'Special') {
-        title.innerHTML = "Special";
-        content.innerHTML = `
-            <li>Exclusive access to VIP areas</li>
-            <li>Private section for guests</li>
-            <li>Top-shelf liquor</li>
+    } else if (package === 'Regular') {
+        content = `
+            <li>General access to the club</li>
+            <li>Variety of drinks</li>
+            <li>Live entertainment</li>
+            <li>Access to all club areas</li>
         `;
     }
+
+    packageInfo.innerHTML = content;
+    modal.style.display = "block";
 }
 
 function closeModal() {
-    document.getElementById("myModal").style.display = "none";
+    const modal = document.getElementById('modal');
+    modal.style.display = "none";
 }
